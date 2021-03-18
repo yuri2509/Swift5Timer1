eeaaf3c6db7ca6b6d9fb9115b2c4874e8816ff2c
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-19 00:25:47 +0900

    Timerメソッドの設定

--- Swift5Timer1/ViewController.swift
+++ Swift5Timer1/ViewController.swift
@@ -17,12 +17,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var stopButton: UIButton!
     
     
+    var timer = Timer()
+    
+    var count = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        count = 0
+        
         //startButtonを押せなくする
         stopButton.isEnabled = true
+        
+    }
+    
+    func startTimer(){
+        
+        //タイマーを回す0.2秒ごとにあるメソッドを呼ぶ
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+    }
+    
+    //0.2秒ごとに呼ばれる
+    @objc func timerUpdate(){
+        
+        count = count + 1
+        imageview.image = UIImage(named: "0")
         
     }
 
